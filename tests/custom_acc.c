8f1f87c99443d9a827702d3f4c303b1939876a41
Sidvetrivel <124465070+Sidvetrivel@users.noreply.github.com>
2024-04-28 21:30:29 -0400

    Update custom_acc.c

--- tests/custom_acc.c
+++ tests/custom_acc.c
@@ -2,35 +2,45 @@
 #include "util.h"
 #include "../maple/api/dcp_maple.h"
 
-#define NUM_WORDS 16
-static uint64_t A,D;
-static uint32_t Dp[2] = {0x000000FF,0x000000AA};
-static uint32_t Ap[NUM_WORDS] = {0x33221100,
-                                                 0x77665544,
-                                                 0xBBAA9988,
-                                                 0xFFEEDDCC,
-                                                 0x11111111,
-                                                 0x22222222,
-                                                 0x33333333,
-                                                 0x44444444,
-                                                 0x55555555,
-                                                 0x66666666,
-                                                 0x77777777,
-                                                 0x88888888,
-                                                 0x99999999,
-                                                 0xAAAAAAAA,
-                                                 0xBBBBBBBB,
-                                                 0xCCCCCCCC};
+#include "../maple/tests/data/matmul_data.h"
 
+#ifndef NUM_A
+#define NUM_A 1
+#endif
+
+#define RUN   (NUM_A * 5)
+#define RUN_1 (RUN + 1)
+// Define hardware-specific operation codes
+#define CMD_START_MULTIPLICATION 0x00000001
+#define CMD_FILLA                0x00000002
+#define CMD_FILLB                0x00000003
+#define CMD_READ_RESULT          0x00000004
 
 #ifndef NUM_A
     #define NUM_A 1
 #endif
 
 void _kernel_(uint32_t id, uint32_t core_num){
-    uint64_t opcode = 0;
-    custom_acc_write(0,opcode,42);
-    print64("Get Acc Result",custom_acc_read(0,opcode));
+
+    // Setup and send data to the accelerator
+    for (uint32_t i = 0; i < 10; i++) {
+        for (uint32_t j = 0; j < 10; j++) {
+            custom_acc_write(0,CMD_FILLA, A_data[i][j]); 
+            custom_acc_write(0,CMD_FILLB, B_data[i][j]);
+        }
+    }
+    
+    // Start the computation on the accelerator
+    custom_acc_write(0, CMD_START_MULTIPLICATION, 0); // Command to start multiplication
+
+    // Assume result matrix is same size as B (for simplicity)
+    uint64_t result;
+    for (uint32_t i = 0; i < 10; i++) {
+        for (uint32_t j = 0; j < 10; j++) {
+            result = custom_acc_read(0, CMD_READ_RESULT); 
+            printf("Result[%d][%d] = %llu\n", i, j, result);
+        }
+    }
 }
 
 int main(int argc, char ** argv) {
